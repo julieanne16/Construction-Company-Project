f9d905a0b71191b73fb0518bd35bba7db926ab29
Reze <seanjerico.1999@gmail.com>
2023-03-16 16:05:45 +0800

    Adding to functions.php
    Adding the displaying of cart function

--- Construction-Project (Frontend)/functions.php
+++ Construction-Project (Frontend)/functions.php
@@ -50,3 +50,50 @@ function register($name, $email, $password)
 		return  "<strong>ERROR: </strong> " . $error->getMessage();
 	}
 }
+
+// GET ROW BY ID
+function getRow($row_id, $id, $table)
+{
+	global $conn;
+
+	try {
+		$stmt = $conn->prepare("SELECT * FROM $table WHERE $row_id = :id");
+		$stmt->execute(array(':id' => $id));
+
+		// Fetch the row as an associative array
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		// Return the row
+		return $row;
+	} catch (PDOException  $error) {
+		return  "<strong>ERROR: </strong> " . $error->getMessage();
+	}
+}
+
+
+// DISPLAY CART
+function displayCart($user_id)
+{
+	global $conn;
+
+	try {
+
+		$stmt = $conn->prepare("SELECT products.product_id, products.img, products.name, products.price, cart.cart_id, cart.quantity, (cart.quantity * products.price) AS total FROM cart INNER JOIN products ON cart.product_id = products.product_id WHERE cart.user_id = :user_id");
+		$stmt->execute(array(":user_id" => $user_id));
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		// If there are rows, return them, otherwise return a message
+		if (!empty($rows)) {
+			$total_amount = 0;
+			foreach ($rows as $row) {
+				$total_amount += $row['total'];
+			}
+			unset($row);
+			return array('rows' => $rows, 'total_amount' => $total_amount,);
+		} else {
+			return array('message' => "There are no items in your cart.", 'total_amount' => 0);
+		}
+	} catch (PDOException  $error) {
+		return  "<strong>ERROR: </strong> " . $error->getMessage();
+	}
+}
