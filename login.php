4f3081e70d011d2fdd9767f26e5d765b2f8db570
Jerome-Nel <100586550+Jerome-Nel@users.noreply.github.com>
2023-03-23 15:20:47 +0800

    add a backend for the login.php

--- login.php
+++ login.php
@@ -1,22 +1,47 @@
-<?php require_once 'includes/header.php'?>
+<?php
 
-    <main>
-        <div class="form">
-		  <h1>SIGN IN</h1>
-          <p>Please fill your email and password to login</p>
-		   <form id="login-form">
-             <div class="form-group">
-                 <label>Email Address</label>
-                 <input type="email" name="email" placeholder="your_mail@website.com">
-             </div>    
-             <div class="form-group">
-                 <label>Password</label>
-                 <input type="password" name="password" placeholder="*******">
-             </div>
+require_once 'includes/header.php';
 
-             <button class="form-btn">SIGN IN</button> 
-             <p>Don't have an account? <a href="#">Register here</a> </p>   
-           </form>
-		</div>
-    </main>
-<?php require_once 'includes/footer.php'?>
+$msg = "";
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+	$user = login($_POST['email'], $_POST['password']);
+
+	if ($user) {
+		// Success login
+		$_SESSION['user_id'] = $user['user_id'];
+		header('Location: products.php');
+		exit();
+	} else {
+		$msg = "Login Failed.";
+	}
+} ?>
+
+<main>
+	<div class="form">
+		<h1>SIGN IN</h1>
+		<p>Please fill your email and password to login</p>
+		<form id="login-form" method="POST">
+
+			<!-- Show message if error -->
+			<?php if ($msg) : ?>
+				<small><?php echo $msg; ?></small>
+			<?php endif; ?>
+			<!-- Show message if error -->
+
+			<div class="form-group">
+				<label>Email Address</label>
+				<input type="email" name="email" placeholder="your_mail@website.com" required>
+			</div>
+			<div class="form-group">
+				<label>Password</label>
+				<input type="password" name="password" placeholder="*******" required>
+			</div>
+
+			<button class="form-btn">SIGN IN</button>
+			<p>Don't have an account? <a href="#">Register here</a> </p>
+		</form>
+	</div>
+</main>
+
+<?php require_once 'includes/footer.php' ?>
