39f111c17829e61f02817912bbf7626c93ff5990
Jerome-Nel <100586550+Jerome-Nel@users.noreply.github.com>
2023-03-23 14:10:14 +0800

    Include a header and footer in the product page

--- products.php
+++ products.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php require_once 'includes/header.php' ?>
 
 <h1>Products</h1>
 
 
-<body>
-    
-
-</body>
 </html>
+
+<?php require_once 'includes/footer.php' ?>
