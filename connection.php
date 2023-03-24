18ab33d5a1d5c14d62508125edfca81ab84c829f
Reze <seanjerico.1999@gmail.com>
2023-03-24 09:56:36 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -10,7 +10,7 @@ $password = '';
 try {
 	$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected successfully";
+	// echo "Connected successfully";
 } catch (PDOException $e) {
 	echo "Connection failed: " . $e->getMessage();
 }
