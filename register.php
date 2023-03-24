feafe3fa1b5cef7100b129ead9a7e249c8de86df
Reze <seanjerico.1999@gmail.com>
2023-03-24 09:39:17 +0800

    Update register.php
    -Added a session that requires the function.php
    -Removed the header and footer tags and replaced it with the header and footer include

--- register.php
+++ register.php
@@ -1,32 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Construction</title>
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
-   <script src="https://kit.fontawesome.com/0ebd665efa.js" crossorigin="anonymous" defer></script>
-</head>
-<body>
-	  <header class="header">
-        <div class="logo"><a href="#">Construction<span> Co.</span></a></div>
-        <navbar class="nav">
-            <a href="#signin">SIGN IN</a>
-            <a href="#contact">CONTACT US</a>
-            <a href="#about">PRODUCTS</a>
-            <a href="#product">ABOUT US</a>
-            <a href="#home">HOME</a>  
-        </navbar>
-        <div class="icon-header">
-            <i class="fa-solid fa-basket-shopping"></i>
-        </div>
-        <div class="menu-btn">
-            <i class="fas fa-bars"></i>
-        </div>
-    </header>
+<?php
 
+session_start();
+
+$msg = "";
+
+require_once 'functions.php';
+
+if (isset($_POST['register'])) {
+	$new_user = register($_POST['name'], $_POST['email'], $_POST['password']);
+	if (!$new_user) {
+		$msg = "This email is already registered.";
+	} else {
+		header('Location: login.php');
+		exit();
+	}
+}
+
+?>
+
+<?php require_once 'includes/header.php'; ?>
     <main>
         <div class="form">
 		  <h1>SIGN UP</h1>
@@ -57,39 +49,7 @@
 		</div>
     </main>
 
-    <footer>
-        <div class="row">
-          <div class="column left">
-            <div class="logo"><a href="#">Construction<span> Co.</span></a></div>
-            <div class="tagline">
-                <p>Tagline of the Construction Company</p>
-            </div>
-            <div class="socmed">
-                <i class="fa fa-facebook-square"></i>
-                <i class="fa fa-instagram"></i>
-                <i class="fa fa-linkedin-square"></i>
-            </div>
-          </div>
-          <div class="column center">
-            <h2>Head Office</h2>
-            <i class='fas fa-map-marker-alt'></i>
-            <p>1017 Vicente Cruz St. Sampaloc <br>Manila, Philippines</p>
-            <i class='fas fa-phone-alt'></i>
-            <p>+632-8831-0000</p>
-            <i class='fas fa-envelope'></i>
-            <p>construction.co@gmail.com</p>
-          </div>
-          <div class="column right">
-            <h2>Legal Info</h2>
-            <p>Cookie Policy</p>
-            <p>Privacy Policy</p>
-            <p>Terms & Condition</p>
-          </div>
-        </div>
-        <div class="footer-bottom">
-            <p>Copyright &copy; Construction Co. | Designed by KAI - Powered by Tech2serv</p>
-        </div>
-    </footer>
-
+    
+<?php require_once 'includes/footer.php' ?>
 </body>
 </html>
