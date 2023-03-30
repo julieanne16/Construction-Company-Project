ec510d32c19ed5b1b8172ff2c4e6590d315fcb2e
Reze <seanjerico.1999@gmail.com>
2023-03-30 20:36:58 +0800

    Added the captcha in the login.php

--- login.php
+++ login.php
@@ -21,7 +21,49 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
 	echo json_encode($account);
 	exit;
-} ?>
+
+
+
+} 
+?>
+
+<?php
+require_once ('connection.php');
+$rand = rand(9999,1000);
+if(isset($_REQUEST['login']))
+{
+
+
+	$email = $_REQUEST['email'];
+	$password = $_REQUEST['password'];
+	$captcha = $_REQUEST['captcha'];
+	$captcharandom = $_REQUEST['captcha-rand'];
+	if($captcha!=$captcharandom)
+	{?>
+		<script type="text/javascript"> alert("Invalid")</script>
+	<?php
+	}
+	else
+	{
+		$select_query = mysql_query($connection, "SELECT * from users where email = '$email' and password = '$password'");
+		$result = mysql_num_rows($select_query);
+		if($result>0)
+	{?>
+			<script type="text/javascript">
+				alert("Login Success");
+			</script>
+<?php
+		}
+		else
+		{?>
+			<script type="text/javascript">
+				alert("Invalid Captcha");
+			</script>
+			<?php
+		}
+	}	
+}
+?>
 
 <?php require_once 'includes/header.php'; ?>
 <?php require_once 'includes/navbar.php'; ?>
@@ -46,6 +88,15 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 				<label for="password">Password</label>
 				<input type="password" class="form-control" id="password" name="password" placeholder="*******">
 				<span class="invalid-feedback"></span>
+			</div>
+			<div class="form-group">
+				<label for="captcha">Captcha</label>
+				<input type="text" class="form-control" name="captcha" id="" placeholder="Enter Captcha" required> 
+				<input type="hidden" name="captcha-rand" value="<?php echo $rand; ?>">
+			</div>
+			<div class="form-group">
+				<label for="captcha" class="form-control" >Captcha Code</label>
+				<div class="captcha" class="form-control" ><?php echo $rand; ?></div> 
 			</div>
 
 			<button type="submit" name="login" id="loginBtn" class="form-btn">SIGN IN</button>
